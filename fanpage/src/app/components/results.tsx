93c11691e5a0977f1e23fdfb6861b9beb40bfeef
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-07-25 18:23:08 +0000

    created publicly-hosted API and tested basic integration of its data as well

--- fanpage/src/app/components/results.tsx
+++ fanpage/src/app/components/results.tsx
@@ -3,14 +3,14 @@ import Breadsticks from "../assets/images/breadsticks.png"
 
 export default function Results(){
     return(
-        <div className="w-full h-[100vh] bg-[#DDBEA9] text-black flex flex-col justify-center items-center text-left">
-            <Image src={Breadsticks} alt="breadsticks" />
+        <section className="w-full h-[100vh] bg-[#DDBEA9] text-black flex flex-col justify-center items-center text-left">
             <div className="text-left">
+                <Image src={Breadsticks} alt="breadsticks" />
                 <h1 className="text-4xl">Breadsticks</h1>
                 <h3 className="text-xl">Unlimited Breadsticks</h3>
                 <p className="text-lg">Our breadsticks are heated up in the microwave by the hundreds.</p>
             </div>
-        </div>
+        </section>
     );
 
 }
--- fanpage/src/app/roulette/page.tsx
+++ fanpage/src/app/roulette/page.tsx
@@ -1,16 +1,64 @@
-import Image from 'next/image'
+'use client'
+import { useState, useEffect } from "react";
 import Spinner from '../components/spinner'
 import Results from '../components/results'
 
+interface MenuItem {
+  alt: string;
+  category: string;
+  description: string;
+  image: string;
+  name: string;
+  price: number;
+  type: string;
+}
+
+async function fetchMenuData() {
+  try {
+    const response = await fetch('/api/menu.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    const menuData = await response.json();
+    return menuData;
+  } catch (error) {
+    console.error('Error fetching menu data:', error);
+    throw error;
+  }
+}
+
 export default function RoulettePage() {
+  const [menu, setMenu] = useState<MenuItem[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const menuData = await fetchMenuData();
+        setMenu(menuData)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        console.log('menu state', menu)
+      }
+    }
+    fetchData();
+  }, []);
+
+
   return (
       <div className="relative bg-white w-full flex flex-col item-center justify-center">
         <Spinner />
-        <div className="text-center text-black py-10 block lg:hidden">
+        <section className="text-center text-black py-10 block lg:hidden">
           <h1 className="text-7xl my-10">Savor The Unexpected</h1>
           <p className="text-3xl">Olive Garden&apos;s Roulette of Culinary Treasures</p>
-        </div>
+        </section>
         <Results />
+        {menu && menu.map((item: MenuItem, index: number) => (
+          <div key={index} className="text-black">
+            <h3>{item.name}</h3>
+            <h5>{item.price}</h5>
+          </div>
+        ))}
       </div>
   )
 }

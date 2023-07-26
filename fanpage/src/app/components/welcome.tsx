29df95ab1bfb7410f078bfae555e19ed11154022
Jimmy Wu <jameswu49@gmail.com>
2023-07-26 21:56:59 +0000

    created carousel component and carousel at the bottom of footer

--- fanpage/src/app/components/welcome.tsx
+++ fanpage/src/app/components/welcome.tsx
@@ -3,10 +3,10 @@ import placeholder from "../assets/images/vin.jpeg"
 
 export default function Welcome() {
     return (
-        <div className="text-center red-hat text-3xl mt-5 lg:h-[90vh]">
+        <section className="text-center red-hat text-3xl mt-5 lg:h-[90vh]">
             <h1>Welcome Home!</h1>
             <p className="text-xl mt-3">Where Family, Friends, and Flavor Meet</p>
             <Image src={placeholder} alt='placeholder' className="px-5 mt-5 lg:w-3/4 lg:h-3/4 lg:mx-auto" />
-        </div>
+        </section>
     )
 }
--- fanpage/src/app/page.tsx
+++ fanpage/src/app/page.tsx
@@ -1,23 +1,24 @@
+"use client"
 import Image from 'next/image'
 import logo from "./assets/images/logo.png"
 import Welcome from './components/welcome'
 import Footer from './components/footer'
-
+import Carousel from './components/carousel'
 
 export default function Home() {
   return (
-    <section className='bg-[#F2E9E4]'>
-      <div className='w-full relative'>
+    <div className='bg-[#F2E9E4]'>
+      <section className='w-full relative'>
         <Image
-          // layout="fill"
-          // objectFit="contain"
           src={logo}
           alt="logo"
           className='lg:w-screen lg:h-screen'
         />
-      </div>
+      </section>
       <Welcome />
       <Footer />
-    </section>
+      <Carousel />
+    </div>
   )
 }
+

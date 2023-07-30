e5ebe96916803fa0049803dbb41d9057e6da27ae
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-07-30 00:35:59 +0000

    created spacer component for flex boxes. adjusted responsive styling to results section

--- fanpage/src/app/components/SpinnerResult.tsx
+++ fanpage/src/app/components/SpinnerResult.tsx
@@ -2,13 +2,14 @@
 import Image from "next/image";
 import Breadsticks from "../assets/images/breadsticks.png"
 import { useRandomItem } from "../contexts/SpinnerResultContext";
+import FlexBasisFull from "./FlexBasisFull";
 
 export default function Results(){
      const { randomItem } = useRandomItem();
 
     return(
         <section className="w-full h-[100vh] bg-[#DDBEA9] text-black flex flex-col justify-center items-center text-left" id="spinner-result">
-            <div className="text-left">
+            <div className="text-left flex flex-wrap px-12 w-full md:w-1/2 xl:w-1/3">
                 {randomItem && (
                     <>
                         <Image
@@ -17,19 +18,27 @@ export default function Results(){
                           width={580}
                           height={580}
                         />
+                        <FlexBasisFull />
                         <h1 className="text-4xl">{randomItem.name}</h1>
-                        <h3 className="text-xl">{randomItem.category}</h3>
+                        <FlexBasisFull />
+                        <h3 className="text-md italic">{randomItem.category}</h3>
+                        <FlexBasisFull />
                         <p className="text-lg">{randomItem.description}</p>
-                        <p className="text-lg">{`Price: ${randomItem.price}`}</p>
+                        <FlexBasisFull />
+                        <p className="text-lg font-bold">{`Price: ${randomItem.price}`}</p>
                     </>
                 )}
                 {!randomItem && (
                 <>
                     <Image src={Breadsticks} alt="breadsticks" />
+                    <FlexBasisFull />
                     <h1 className="text-4xl">Unlimited Breadsticks!</h1>
-                    <h3 className="text-xl">Appetizer</h3>
+                    <FlexBasisFull />
+                    <h3 className="text-md italic">Appetizer</h3>
+                    <FlexBasisFull />
                     <p className="text-lg">Our breadsticks are heated up in the microwave by the hundreds.</p>
-                    <p className="text-lg">Price: FREE UNLIMITED!!</p>
+                    <FlexBasisFull />
+                    <p className="text-lg font-bold">Price: FREE UNLIMITED!!</p>
                 </>)}
             </div>
         </section>

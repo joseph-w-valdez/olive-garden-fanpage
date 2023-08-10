cf9057bd65947b47cc2650ef0d59f41f851700c0
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-08-10 17:49:45 +0000

    fixed welcome image responsiveness

--- fanpage/src/app/components/Welcome.tsx
+++ fanpage/src/app/components/Welcome.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image"
-import placeholder from "../assets/images/vin.jpeg"
+import vin from "../assets/images/vin.jpeg"
 
 export default function Welcome() {
     return (
         <section className="flex flex-col items-center text-center red-hat text-3xl mt-5 lg:h-minus-navbar">
             <h1>Welcome Home!</h1>
             <p className="text-xl mt-3">Where Family, Friends, and Flavor Meet</p>
-            <div className="w-full max-w-[1400px] h-fit landscape-sm:w-1/2 pt-6">
+            <div className="w-full max-w-[1250px] h-fit pt-6">
                 <Image
-                    src={placeholder}
-                    alt='placeholder'
+                    src={vin}
+                    alt='vin welcomes you to the family'
                     width={0}
                     height={0}
                     sizes='100vw'

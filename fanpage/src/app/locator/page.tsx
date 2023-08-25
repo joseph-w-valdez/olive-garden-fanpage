5b996c6c42e51b8d680e1f5ea095cdc8e1073369
JordanTDawson <jtdawson@icloud.com>
2023-08-25 17:40:54 +0000

    Setup conversion for longitude and latitude.

--- fanpage/src/app/locator/page.tsx
+++ fanpage/src/app/locator/page.tsx
@@ -1,28 +1,25 @@
 'use client'
 import { GoogleMap, InfoWindowF, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 import { useEffect, useRef, useState } from "react";
+const googleApiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY!;
+if (!googleApiKey) {
+    throw new Error("Google API key is not defined in your environment.")
+;}
 
 const containerStyle = {
     width: '100%',
-    height: '400px'
+    height: '1080px'
 };
 
 async function getLatLonforCity(city: string) {
     const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
         city + ', USA'
-    )}&key=${process.env.GOOGLE_MAPS_PUBLIC_API_KEY!}`;
+    )}&key=${googleApiKey}`;
     const geocodeResponse = await fetch(geocodeUrl);
     const geocodeData = await geocodeResponse.json();
     const { lat, lng } = geocodeData.results[0].geometry.location;
     return { lon: lng, lat};
 }
-
-export type Place = {
-    name: string;
-    address: string;
-    latitude: number;
-    longitude: number;
-};
 
 export default function Locator() {
     const [searchText, setSearchText] = useState<string>('');
@@ -32,12 +29,28 @@ export default function Locator() {
 
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+        googleMapsApiKey: googleApiKey,
     })
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if(!searchText) {
+            return console.log('No search text?')
+        }
         setIsLoading(true);
+
+        try {
+            const locationData = await getLatLonforCity(searchText);
+            console.log(locationData);
+        } catch (error) {
+            console.error('Error fetching location data:', error);
+        } finally {
+            setIsLoading(false);
+            setSearchText('');
+            if (inputRef.current) {
+                inputRef.current.focus();
+            }
+        }
         if (inputRef.current === null) return
         console.log('Submitted:', searchText);
         setTimeout(() => {
@@ -46,32 +59,34 @@ export default function Locator() {
     };
 
     return (
-        
-        <div className='w-full flex flex-wrap item-center justify-center'>
-            <h1>Hello World!</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    required
-                    ref={inputRef}
-                    value={searchText}
-                    onChange={(event) => setSearchText(event.target.value)}
-                    placeholder="Enter your search"
-                />
-                <button
-                    disabled={isLoading}
-                    >
-                    {isLoading ? 'Loading...' : 'Search'}
+        <>
+            <div className='w-full flex flex-col item-center justify-center'>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        required
+                        ref={inputRef}
+                        value={searchText}
+                        onChange={(event) => setSearchText(event.target.value)}
+                        placeholder="Enter your search"
+                    />
+                    <button
+                        disabled={isLoading}
+                        >
+                        {isLoading ? 'Loading...' : 'Search'}
                     </button>
-            </form>
-            {isLoaded && (
-                <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={{ lat:33.69383958039442, lng:-117.83629870284751}}
-                zoom={13}
-                >
-                </GoogleMap>
-            )}
-        </div>
+                </form>
+            </div>
+            <div>
+                {isLoaded && (
+                    <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={{ lat:33.69383958039442, lng:-117.83629870284751}}
+                    zoom={14}
+                    >
+                    </GoogleMap>
+                )}
+            </div>
+        </>
     );
 }
